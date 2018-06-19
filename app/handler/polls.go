d3abe3b105d87b24ff79d9216a40dee0d0d6fa49
drklee3 <dlee3@scu.edu>
2018-06-19 11:21:18 -0700

    Add db handlers for each endpoint

--- app/handler/polls.go
+++ app/handler/polls.go
@@ -38,11 +38,8 @@ func CreatePoll(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
 
 // GetPoll gets a single poll
 func GetPoll(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-
 	// get the poll item
-	poll, err := getPoll(db, id, w, r)
+	poll, err := getPoll(db, w, r)
 	if err != nil {
 		log.Printf("error: %s", err)
 		return
@@ -51,23 +48,84 @@ func GetPoll(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
 	respondJSON(w, http.StatusOK, poll)
 }
 
+// UpdatePoll updates poll options
 func UpdatePoll(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	poll, err := getPoll(db, w, r)
+	if err != nil {
+		log.Printf("error: %s", err)
+		return
+	}
 
+	// decode body
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&poll); err != nil {
+		respondError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	defer r.Body.Close()
+
+	// save new poll
+	if err := db.Save(&poll).Error; err != nil {
+		respondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondJSON(w, http.StatusOK, poll)
 }
 
+// DeletePoll deletes a single poll
 func DeletePoll(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	poll, err := getPoll(db, w, r)
+	if err != nil {
+		log.Printf("error: %s", err)
+		return
+	}
 
+	if err := db.Delete(&poll).Error; err != nil {
+		respondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondJSON(w, http.StatusNoContent, nil)
 }
 
+// ArchivePoll disables further submissions for a single poll
 func ArchivePoll(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	poll, err := getPoll(db, w, r)
+	if err != nil {
+		log.Printf("error: %s", err)
+		return
+	}
 
+	poll.Archive()
+	if err := db.Save(&poll).Error; err != nil {
+		respondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondJSON(w, http.StatusOK, poll)
 }
 
+// RestorePoll re-enables submissions for a single poll
 func RestorePoll(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	poll, err := getPoll(db, w, r)
+	if err != nil {
+		log.Printf("error: %s", err)
+		return
+	}
 
+	poll.Restore()
+	if err := db.Save(&poll).Error; err != nil {
+		respondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondJSON(w, http.StatusOK, poll)
 }
 
-func getPoll(db *gorm.DB, idStr string, w http.ResponseWriter, r *http.Request) (*model.Poll, error) {
+// getPoll gets a single poll by id and responds with 404 if not found
+func getPoll(db *gorm.DB, w http.ResponseWriter, r *http.Request) (*model.Poll, error) {
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+
 	// parse id from string
 	id, err := strconv.ParseUint(idStr, 10, 64)
 	if err != nil {
